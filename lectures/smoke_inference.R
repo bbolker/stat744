532c6bfc067f07e3e137cd70161af670457b3e0d
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-02-04 13:40:13 -0500

    Resting

---
+++ lectures/smoke_inference.R
@@ -0,0 +1,29 @@
+## par(ps=20)
+library(coefplot2)
+library(dplyr)
+
+smoke <- smoke %>% rename(smoking=smoke)
+summary(smoke)
+full <- lm(fev ~ age + height + sex + smoking, data=smoke)
+
+coefplot2(full)
+
+stdsmoke <- (smoke
+	%>% transmute(fev = fev/sd(fev)
+		, age = age/sd(age)
+		, height = height/sd(height)
+		, sex = as.numeric(as.factor(sex))
+		, sex = sex/sd(sex)
+		, smoking = as.numeric(as.factor(smoking))
+		, smoking = smoking/sd(smoking)
+	)
+)
+
+print(stdsmoke)
+
+std <- lm(smoke$fev ~ age + height + sex + smoking, data=stdsmoke)
+coefplot2(std, main="Standardized effect on fev (L/s)")
+
+partial <- lm(fev ~ age + height + sex + smoking, data=stdsmoke)
+coefplot2(partial, main="Partial correlations with fev")
+
---
+++ lectures/uni.R
@@ -0,0 +1,15 @@
+
+uni <- lm(fev ~ height, data=smoke)
+pointPred <- as_data_frame(predict(uni, interval="confidence"))
+termPred <- predict(uni, interval="confidence", type="terms")
+
+base <- ggplot(smoke, aes(height, fev))
+
+print(scatter <- base + geom_point(alpha=0.1))
+
+print(pp <- scatter 
+	+ geom_line(aes(y=pointPred$fit))
+	+ geom_line(aes(y=pointPred$lwr), lty=2)
+	+ geom_line(aes(y=pointPred$upr), lty=2)
+)
+
