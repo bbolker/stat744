e857984297b20065c981c209fde3f82ce2fec728
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-09-09 23:40:22 -0400

    Working on lecture in a bad place

--- lectures/orchard.R
+++ lectures/orchard.R
@@ -7,6 +7,7 @@ data("OrchardSprays")
 base <- ggplot(OrchardSprays,aes(x=treatment,y=decrease))
 lbase <- base +  scale_y_log10()
 
+## Bar charts, lin and log
 print(bar <- base
     +  stat_summary(fun.data=mean_cl_normal,geom="bar",colour="gray")
     +  stat_summary(fun.data=mean_cl_normal,geom="errorbar",width=0.5)
@@ -14,10 +15,16 @@ print(bar <- base
 
 print(bar +  scale_y_log10())
 
+## pointrange is similar to error bar; these are are the non-dynamite analogues of the above
+print(base 
+	+ stat_summary(fun.data=mean_cl_normal,geom="pointrange")
+)
+
 print(lbase 
 	+ stat_summary(fun.data=mean_cl_normal,geom="pointrange")
 )
 
+## Now we have just sds, I guess
 print(lbase 
 	+ stat_summary(fun.data=mean_sdl,geom="pointrange")
 )
@@ -27,8 +34,9 @@ print(base
 )
 
 print(lbase + geom_point())
-
-## Hybrid of data and inference; is it good?
-##print(lrange + geom_point(color="blue", alpha=0.3))
 print(lbase + geom_boxplot())
 print(lbase + geom_violin())
+
+## Old idea, oldly suppressed as well
+## Hybrid of data and inference; is it good?
+##print(lrange + geom_point(color="blue", alpha=0.3))
